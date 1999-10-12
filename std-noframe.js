bf9bbdfb42babb4f8f889088201debf99af5584b
sairi-na-tenshi <sairi.na.tenshi@gmail.com>
1999-10-12 19:38:17 +0400

    renaming simplification

--- std-noframe.js
+++ std-noframe.js
@@ -1,16 +1,13 @@
-CComponent( 'i:noframe', function( el ){
+CComponent( 'std:noframe', function( el ){
 
-	var nodeLoader= FLazy( function(){
-		var node= el.getElementsByTagName( 'iframe' )[0]
+	var nodeRoot= FValue( el )
+	var nodeFrame= FLazy( function(){
+		var node= nodeRoot().getElementsByTagName( 'iframe' )[0]
 		return FValue( node )
 	})
-	var nodeContent= FLazy( function(){
-		var node= document.createElement( 'i:noframe-content' )
-		el.appendChild( node )
-		return FValue( node )
-	})
+
 	var docLoaded= function(){
-    	return nodeLoader().contentWindow.document
+    	return nodeFrame().contentWindow.document
 	}
 	var bodyLoaded= function(){
     	var doc= docLoaded()
@@ -18,31 +15,30 @@ CComponent( 'i:noframe', function( el ){
     	return body
 	}
 
-    var updateContent= function(){
+    var dissolve= function(){
     	var body= bodyLoaded()
-    	var content= nodeContent()
-    	content.innerHTML= ''
+    	var root= nodeRoot()
+    	var parent= root.parentNode
     	var frag= docLoaded().createDocumentFragment()
 		var child; while( child= body.firstChild ) frag.appendChild( child )
-		content.appendChild( frag )
-		el.className+= ' loaded=true'
-		var hrefLoaded= docLoaded().location.href
+		parent.insertBefore( frag, root )
+		parent.removeChild( root )
+
 		var hrefCurrent= document.location.href
 		var links= document.links
 		for( var i= 0; i < links.length; ++i ){
 			var link= links[i]
 			if( link.href === hrefCurrent ) link.className+= ' target=true'
-			if( link.target !== nodeLoader().name ) continue
-			if( link.href === hrefLoaded ) link.className+= ' target=true'
 			else link.className= link.className.replace( /\btarget=true\b/, '' )
 		}
-		document.title= docLoaded().title
     }
 
-    var loader= nodeLoader()
-    loader.attachEvent
-    	? loader.attachEvent( 'onload', updateContent )
-    	: loader.onload= updateContent
-    if( bodyLoaded() ) updateContent()
+    if( bodyLoaded() ){
+    	dissolve()
+    } else {
+	    var frame= nodeFrame()
+	    if( frame.attachEvent ) frame.attachEvent( 'onload', dissolve )
+	    else frame.onload= dissolve
+	}
 
 })
